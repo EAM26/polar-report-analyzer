1090623a278f6f056ec590b4b3af7f4a5ec69de5
emile <emuller2607@gmail.com>
2026-03-16 14:29:10 +0100

    first commit

---
+++ src/main/java/org/eamcode/polarreportanalyzer/PolarReportAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package org.eamcode.polarreportanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarReportAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarReportAnalyzerApplication.class, args);
+	}
+
+}
