f7fe4522f43ebbf38552f977df9c33731e4a17ff
emile <emuller2607@gmail.com>
2026-03-19 11:39:31 +0100

    Define remaining fields in DataPointService

--- src/main/java/org/eamcode/polarreportanalyzer/service/DataPointService.java
+++ src/main/java/org/eamcode/polarreportanalyzer/service/DataPointService.java
@@ -18,7 +18,14 @@ public class DataPointService {
             dataPoint.setRelativeSecond(i-3);
             dataPoint.setTimeStamp(row[1].isEmpty() ? null : LocalTime.parse(row[1]));
             dataPoint.setHeartRate(row[2].isEmpty() ? null : Integer.parseInt(row[2]));
-          
+            dataPoint.setSpeed(row[3].isEmpty() ? null : Double.parseDouble(row[3]));
+//            dataPoint.setPace(row[4].isEmpty() ? null : Double.parseDouble(row[4]));
+            dataPoint.setCadence(row[5].isEmpty() ? null : Integer.parseInt(row[5]));
+            dataPoint.setAltitude(row[6].isEmpty() ? null : Double.parseDouble(row[6]));
+            dataPoint.setStrideLength(row[7].isEmpty() ? null : Double.parseDouble(row[7]));
+            dataPoint.setDistance(row[8].isEmpty() ? null : Double.parseDouble(row[8]));
+            dataPoint.setTemperature(row[9].isEmpty() ? null : Double.parseDouble(row[9]));
+            dataPoint.setPower(row[10].isEmpty() ? null : Integer.parseInt(row[10]));
 
             training.addToDataPoint(dataPoint);
         }
