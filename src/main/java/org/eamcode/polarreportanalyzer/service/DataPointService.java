190b29247f8ad31637224d4e620f5f95c02b058a
emile <emuller2607@gmail.com>
2026-03-25 10:25:23 +0100

    Create CsvParsingException with GlobalExceptionHandler to handle numberformat parser problems

--- src/main/java/org/eamcode/polarreportanalyzer/service/DataPointService.java
+++ src/main/java/org/eamcode/polarreportanalyzer/service/DataPointService.java
@@ -1,6 +1,7 @@
 package org.eamcode.polarreportanalyzer.service;
 
 import org.eamcode.polarreportanalyzer.dto.PhaseRequest;
+import org.eamcode.polarreportanalyzer.exception.CsvParsingException;
 import org.eamcode.polarreportanalyzer.model.DataPoint;
 import org.eamcode.polarreportanalyzer.model.Training;
 import org.eamcode.polarreportanalyzer.repository.DataPointRepository;
@@ -26,22 +27,30 @@ public class DataPointService {
         for (int i = 3; i < dataRows.size(); i++) {
             DataPoint dataPoint = new DataPoint();
             String[] row = dataRows.get(i);
-            dataPoint.setRelativeSecond(i-3);
-            dataPoint.setTimeStamp(row[1].isEmpty() ? null : LocalTime.parse(row[1]));
-            dataPoint.setHeartRate(row[2].isEmpty() ? null : Integer.parseInt(row[2]));
-            dataPoint.setSpeed(row[3].isEmpty() ? null : Double.parseDouble(row[3]));
+//Todo create try/catch block to handle custom CsvParserException
+            try {
+                dataPoint.setTimeStamp(row[1].isEmpty() ? null : LocalTime.parse(row[1]));
+                dataPoint.setHeartRate(row[2].isEmpty() ? null : Integer.parseInt(row[2]));
+                dataPoint.setSpeed(row[3].isEmpty() ? null : Double.parseDouble(row[3]));
 
-            Integer paceInSeconds = row[4].isEmpty() ? null : dateTimeFormatter.formatToSeconds(row[4]);
-            dataPoint.setPaceSeconds(paceInSeconds);
+                Integer paceInSeconds = row[4].isEmpty() ? null : dateTimeFormatter.formatToSeconds(row[4]);
+                dataPoint.setPaceSeconds(paceInSeconds);
 
-            dataPoint.setCadence(row[5].isEmpty() ? null : Integer.parseInt(row[5]));
-            dataPoint.setAltitude(row[6].isEmpty() ? null : Double.parseDouble(row[6]));
+                dataPoint.setCadence(row[5].isEmpty() ? null : Integer.parseInt(row[5]));
+                dataPoint.setAltitude(row[6].isEmpty() ? null : Double.parseDouble(row[6]));
             dataPoint.setStrideLength(row[7].isEmpty() ? null : Double.parseDouble(row[7]));
-            dataPoint.setDistance(row[8].isEmpty() ? null : Double.parseDouble(row[8]));
-            dataPoint.setTemperature(row[9].isEmpty() ? null : Double.parseDouble(row[9]));
-            dataPoint.setPower(row[10].isEmpty() ? null : Integer.parseInt(row[10]));
+                dataPoint.setDistance(row[8].isEmpty() ? null : Double.parseDouble(row[8]));
+                dataPoint.setTemperature(row[9].isEmpty() ? null : Double.parseDouble(row[9]));
+                dataPoint.setPower(row[10].isEmpty() ? null : Integer.parseInt(row[10]));
+            } catch (NumberFormatException e) {
+                throw new CsvParsingException("Invalid value in row: " + i, e);
+            }
 
-            training.addToDataPoint(dataPoint);
+                dataPoint.setRelativeSecond(i-3);
+
+                training.addToDataPoint(dataPoint);
+
+
         }
     }
 
--- src/main/java/org/eamcode/polarreportanalyzer/util/CsvReader.java
+++ src/main/java/org/eamcode/polarreportanalyzer/util/CsvReader.java
@@ -15,27 +15,6 @@ import java.util.List;
 @Component
 public class CsvReader {
 
-//    public List<String[]> readMetaData(String resourcePath) throws IOException, CsvValidationException {
-//        List<String[]> firstTwoRows = new ArrayList<>();
-//
-//        ClassPathResource resource = new ClassPathResource(resourcePath);
-//
-//        try (Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
-//             CSVReader csvReader = new CSVReader(reader)) {
-//
-//            String[] header = csvReader.readNext();
-//            if (header != null) {
-//                firstTwoRows.add(header);
-//            }
-//
-//            String[] values = csvReader.readNext();
-//            if (values != null) {
-//                firstTwoRows.add(values);
-//            }
-//        }
-//        return firstTwoRows;
-//    }
-
     public List<String[]> readDataRows(String resourcePath)  {
 
         ClassPathResource resource = new ClassPathResource(resourcePath);
